14b11d1cc99c2d7cd24df0cce60574ecac6e0a42
JPbonaldo <159311815+JPbonaldo@users.noreply.github.com>
2024-03-21 00:28:02 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let array = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io", ".etc"];
+  let domainString = "";
+  var a, b, c, d, e;
+
+  for (a = 0; a < pronoun.length; a++) {
+    for (b = 0; b < adj.length; b++) {
+      for (c = 0; c < noun.length; c++) {
+        for (d = 0; d < extension.length; d++) {
+          domainString = pronoun[a] + adj[b] + noun[c] + extension[d];
+          array.push(domainString);
+        }
+      }
+    }
+  }
+
+  for (e = 0; e < array.length; e++) {
+    let newElement = document.createElement("h6");
+    newElement.innerHTML = array[e];
+    document.querySelector("#domainName").appendChild(newElement);
+  }
 };
